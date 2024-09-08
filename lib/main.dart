92100fda01734c26ef73ff2868ca4ecb366da69c
Moura196 <gmendesmoura@gmail.com>
2024-09-08 14:17:16 -0300

    Finalizando realização da prova, criando outras funções.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
-import 'dart:io';
+import 'package:google_fonts/google_fonts.dart';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
+    // theme: ThemeData(
+    //   textTheme: GoogleFonts.pixelifySansTextTheme(),
+    // ), 
+    // _buildTheme(Brightness.light),
   ));
 }
+
+// ThemeData _buildTheme(brightness) {
+//   var baseTheme = ThemeData(brightness: brightness);
+
+//   return baseTheme.copyWith(
+//     textTheme: GoogleFonts.pixelifySansTextTheme(baseTheme.textTheme),
+//   );
+// }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -50,116 +61,240 @@ class _HomePageState extends State<HomePage> {
         (minhaEscolha == 'Pedra' && escolhaPC == 'Tesoura') ||
         (minhaEscolha == 'Papel' && escolhaPC == 'Pedra') ||
         (minhaEscolha == 'Tesoura' && escolhaPC == 'Papel')) {
-          print(resultado);
           resultado = 'Você ganhou!';
           meuPlacar++;
+          corTexto(resultado);
       } else {
-        resultado = "Você perdeu!";
+        resultado = 'Você perdeu!';
         placarPC++;
+        corTexto(resultado);
       }
     });
+  }
+
+  Color corTexto(String _resultado) {
+    if (_resultado == 'Você ganhou!') {
+      return Colors.green;
+    } else if (_resultado == 'Você perdeu!') {
+      return Colors.red;
+    } else {
+      return Colors.black;
+    }
+  }
+
+  Color corFundo() {
+    if (meuPlacar > placarPC) {
+      return Colors.green;
+    } else if (meuPlacar < placarPC) {
+      return Colors.red;
+    } else {
+      return Colors.grey;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.greenAccent[700],
         title: Center(
-          child: Text('Prova 1 - Pedra, Papel e Tesoura')),
+          child: Text(
+            'Prova 1 - Pedra, Papel e Tesoura',
+            style: GoogleFonts.pixelifySans(
+              textStyle: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('Sua Jogada'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  child: Center(
-                    child: GestureDetector(
-                      child: TextButton(
-                        onPressed: () {comecarJogo('Pedra');},
-                        child: Text('Pedra'))
-                    )
-                  ),
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[350],
-                    borderRadius: BorderRadius.circular(50),
+        child: Container(
+          color: Colors.grey[400],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Sua Jogada',
+                style: GoogleFonts.pixelifySans(
+                  textStyle: TextStyle(
+                    fontSize: 25,
                   ),
                 ),
-                Container(
-                  child: Center(
-                    child: GestureDetector(
-                      child: TextButton(
-                        onPressed: () {comecarJogo('Papel');},
-                        child: Text('Papel')),
-                    )
-                  ),
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[350],
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-                Container(
-                  child: Center(
-                    child: TextButton(
-                      onPressed: () {comecarJogo('Tesoura');},
-                      child: Text('Tesoura')),
-                  ),
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[350],
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-              ]
-            ),
-            Text('Jogada do Computador'),
-            Container(
-              child: Center(
-                child: Text('$escolhaPC')),
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.grey[350],
-                borderRadius: BorderRadius.circular(50),
               ),
-            ),
-            Text('Resultado: $resultado'),
-            Container(
-              width: 250,
-              height: 90,
-              decoration: BoxDecoration(
-                color: Colors.grey[350],
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Você'),
-                      Text('$meuPlacar')
-                    ]
+                  Container(
+                    child: Center(
+                      child: GestureDetector(
+                        child: TextButton(
+                          onPressed: () {comecarJogo('Pedra');},
+                          child: Text(
+                            'Pedra',
+                            style: GoogleFonts.pixelifySans(
+                              textStyle: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          )
+                        )
+                      )
+                    ),
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[350],
+                      borderRadius: BorderRadius.circular(50),
+                    ),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('PC'),
-                      Text('$placarPC')
-                    ],
+                  Container(
+                    child: Center(
+                      child: GestureDetector(
+                        child: TextButton(
+                          onPressed: () {comecarJogo('Papel');},
+                          child: Text(
+                            'Papel',
+                            style: GoogleFonts.pixelifySans(
+                              textStyle: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          )
+                        ),
+                      )
+                    ),
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[350],
+                      borderRadius: BorderRadius.circular(50),
+                    ),
                   ),
-                ],
+                  Container(
+                    child: Center(
+                      child: TextButton(
+                        onPressed: () {comecarJogo('Tesoura');},
+                        child: Text(
+                          'Tesoura',
+                          style: GoogleFonts.pixelifySans(
+                              textStyle: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          )
+                        ),
+                    ),
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[350],
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ]
               ),
-            )
-          ] 
+              Text(
+                'Jogada do Computador',
+                style: GoogleFonts.pixelifySans(
+                  textStyle: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text(
+                    '$escolhaPC',
+                    style: GoogleFonts.pixelifySans(
+                              textStyle: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                  )
+                ),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.grey[350],
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              Text(
+                'Resultado',
+                style: GoogleFonts.pixelifySans(
+                  textStyle: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Text(
+                '$resultado',
+                style: GoogleFonts.pixelifySans(
+                  textStyle: TextStyle(
+                    color: corTexto(resultado),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Container(
+                width: 250,
+                height: 90,
+                decoration: BoxDecoration(
+                  color: corFundo(),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Você',
+                          style: GoogleFonts.pixelifySans(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '$meuPlacar',
+                          style: GoogleFonts.pixelifySans(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      ]
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'PC',
+                          style: GoogleFonts.pixelifySans(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '$placarPC',
+                          style: GoogleFonts.pixelifySans(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ] 
+          ),
         )
       ),
     );
